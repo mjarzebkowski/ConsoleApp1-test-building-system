9c392f781cd4d9a8b2618b5dfc6684fd2688e8b9
MJ-Komputer\MJ <mjarzebkowski>
2017-09-23 19:17:49 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1-test-building-system/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1_test_building_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
